70c1a81667b55e626028f8ef186d33c64603f437
Emre Neftci <eneftci@uci.edu>
2019-09-18 01:15:01 -0700

    initial commits

---
+++ pytorch_datasets/ntidigits/ntidigits_dataloaders.py
@@ -0,0 +1,119 @@
+import struct
+import numpy as np
+import scipy.misc
+import h5py
+import glob
+import os
+import torch.utils.data
+from ..events_timeslices import frame_evs, one_hot
+
+def one_hot1d(mbt, num_classes):
+    out = np.zeros([num_classes], dtype='float32')
+    out[int(mbt)] = 1
+    return out
+
+def make_infinite(dataloader):
+    while True:
+        for x in iter(dataloader):
+            yield x
+            
+class NTIdigitsDataset(torch.utils.data.Dataset):
+    nclasses = 11
+    
+    def __init__(self, evs, labels, ds=[1], size=[64], dt=1000, max_duration=1000):
+        super(NTIdigitsDataset).__init__()
+        self.evs = evs
+        self.labels = labels
+        self.ds = ds
+        self.dt = dt
+        self.size = size
+        self.max_duration = max_duration
+
+    def __getitem__(self, key):
+        tm = self.evs[key][:,0]
+        ad = self.evs[key][:,1:2]
+        #T = np.minimum(tm[-1]//self.dt,self.max_duration)
+        T = self.max_duration
+        sample = frame_evs(tm, ad, duration=T, downsample=[self.ds], size=self.size, deltat=self.dt)
+        learn_mask = np.convolve(np.ones(30)/30,sample.reshape(-1,self.size[0]).sum(axis=1),mode='same')>.1
+        targets = np.ones(T)*11
+        targets[learn_mask] = self.labels[key]
+        return np.array(sample, dtype='float32'), one_hot(targets, self.nclasses+1)[:,:self.nclasses].astype('float32')
+    
+    def __len__(self):
+        return len(self.evs)
+        
+
+def create_data(filename = 'data/tidigits/n-tidigits.hdf5',
+                chunk_size_train=10,
+                chunk_size_test=20,
+                batch_size=50,
+                size=[2, 128, 128],
+                dt = 1000,
+                ds = 1,
+                **dl_kwargs):
+    
+    """
+    chunk_size_train: number of samples in the time axis for the training set
+    chunk_size_test: number of samples in the time axis for the testing set
+    batch_size: batch_size on each iteration
+    size: expected input size, list in the format [channels, dimx]
+    dt: framing delta t in microseconds
+    ds: downsampling factor in the dimx dimension. 1 means no downsampling
+    """
+    if not os.path.isfile(filename):
+        raise Exception("File {} does not exist".format(filename))
+
+    f = h5py.File(filename, 'r', swmr=True, libver="latest")
+    #strain = SequenceGenerator(filename, group='train', batch_size = batch_size, chunk_size = seq_len_train, size = size, ds = ds, dt= dt)
+    #stest = SequenceGenerator(filename, group='test', batch_size = batch_size, chunk_size =seq_len_test, size = size, ds = ds, dt= dt)
+    train_labels_isolated = []
+    test_labels_isolated  = []
+    train_evs = []
+    test_evs = []
+    
+    for tl in f['train_labels']:
+        label_ = tl.decode()
+        label_s = label_.split('-')
+        if len(label_s[-1])==1:
+            digit = label_s[-1]
+            if digit is 'o':
+                digit=10
+            if digit is 'z':
+                digit=0
+            else:
+                digit=int(digit)
+            train_labels_isolated.append(digit)
+            tm = np.int32(f['train_timestamps'][label_][:]*1e6)
+            ad = np.int32(f['train_addresses'][label_].value)
+            train_evs.append(np.column_stack([tm,ad]))
+            
+    for tl in f['test_labels']:
+        label_ = tl.decode()
+        label_s = label_.split('-')
+        if len(label_s[-1])==1:
+            digit = label_s[-1]
+            if digit is 'o':
+                digit=10
+            if digit is 'z':
+                digit=0
+            else:
+                digit=int(digit)
+            test_labels_isolated.append(digit)
+            tm = np.int32(f['test_timestamps'][label_][:]*1e6)
+            ad = np.int32(f['test_addresses'][label_].value)
+            test_evs.append(np.column_stack([tm,ad]))
+    
+    f.close()
+            
+    train_d = NTIdigitsDataset(train_evs, train_labels_isolated, ds = ds, size = size, dt = dt, max_duration = chunk_size_train)
+    train_dl = torch.utils.data.DataLoader(train_d,
+                                           batch_size=batch_size,
+                                           shuffle=True, **dl_kwargs)
+    
+    test_d   = NTIdigitsDataset(test_evs, test_labels_isolated, ds = ds, size = size, dt = dt, max_duration = chunk_size_test)
+    test_dl = torch.utils.data.DataLoader( test_d,
+                                           batch_size=batch_size,
+                                           shuffle=True, **dl_kwargs)
+
+    return train_dl, test_dl
---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+DCLL library
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = "pytorch_datasets",
+    version = "0.1",
+    author = "Emre Neftci",
+    author_email = "eneftci@uci.edu",
+    description = ("Dataset loaders for pytorch"),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    test_suite = 'nose.collector',
+    long_description=long_description,
+    license='Apache License 2.0',
+    install_requires=[
+        "torch>=0.4",
+        "scipy>=1.0",
+        "h5py"
+    ]
+)
