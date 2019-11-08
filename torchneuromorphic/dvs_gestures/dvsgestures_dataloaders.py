664cc7ebf13f5b165fc4fcd1c470e80f28bfd2ec
Emre <eneftci@uci.edu>
2019-11-08 16:58:46 +0100

    none

--- torchneuromorphic/dvs_gestures/dvsgestures_dataloaders.py
+++ torchneuromorphic/dvs_gestures/dvsgestures_dataloaders.py
@@ -14,7 +14,7 @@ import numpy as np
 import scipy.misc
 import h5py
 import torch.utils.data
-import .create_hdf5
+from .create_hdf5 import create_events_hdf5
 from ..neuromorphic_dataset import NeuromorphicDataset 
 from ..events_timeslices import *
 from ..transforms import *
